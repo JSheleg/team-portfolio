3eaa4ee08305792c4ed9f5f6d6b76e3dbcbe8ddb
JSheleg <jessica.sheleg@gmail.com>
2021-07-16 20:32:25 -0500

    connected page template to create team.html

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,6 @@
-const generateTeamCards = (employees) => {
+const buildTeam = (team) => {
+
+    //create Manager card
     const generateManager = (manager) =>{
         return `
         <div class="col-4 mt-4">
@@ -16,7 +18,8 @@ const generateTeamCards = (employees) => {
         </div>
     `;
     };
-
+    
+    //create Engineer Card
     const generateEngineer = (engineer) => {
         return `
         <div class="col-4 mt-4">
@@ -35,6 +38,7 @@ const generateTeamCards = (employees) => {
     `;
     };
 
+    //create Intern Card
     const generateIntern = (intern) => {
         return `
         <div class="col-4 mt-4">
@@ -52,7 +56,43 @@ const generateTeamCards = (employees) => {
         </div>
     `;
     };
+    //employee array, filter for manager
+    // manager into generateManager(map if multiple)
+    //push to teamArr
+    const teamArr = [];
+    
+    // teamArr.push(team.filter)
+    return teamArr.join("");
 }
+   module.exports = team => { 
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <div class="container flex-row justify-space-between align-center py-3">
+                <h1 class="page-title text-secondary bg-dark py-2 px-3">My Team</h1>
+            </div>
+        </header>
+        <main class="container my-5">
+            ${buildTeam(team)}
+        </main>
+    </body>
+    </html>
+    `;
+   }
+
+    
+
 
 
 
